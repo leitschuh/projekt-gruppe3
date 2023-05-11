48681e93b6d6a08794108fdb9c3ba48daced6cfc
maxil <maxi.leitschuh@gmail.com>
2023-05-11 13:40:45 +0200

    SQL Query angepasst

--- read_from_bigquery.py
+++ read_from_bigquery.py
@@ -16,16 +16,9 @@ def main(argv=None):
     pipeline_options = PipelineOptions(pipeline_args)
 
     input_query = """
-    SELECT trip_id,			
-    subscriber_type,	
-    bikeid,
-    TIMESTAMP_ADD(DATETIME(start_time), INTERVAL 1 YEAR) as `timestamp`,
-    start_station_id,
-    start_station_name,
-    FROM `bigquery-public-data.austin_bikeshare.bikeshare_trips` 
-    where TIMESTAMP_ADD(DATETIME(start_time), INTERVAL 1 YEAR) < CURRENT_DATE()
-    and TIMESTAMP_ADD(DATETIME(start_time), INTERVAL 1 YEAR) > TIMESTAMP_SUB(CURRENT_DATE(), INTERVAL 7 DAY)
-    order by TIMESTAMP_ADD(DATETIME(start_time), INTERVAL 1 YEAR) asc
+    SELECT *
+    FROM `bigquery-public-data.san_francisco.sfpd_incidents` 
+    order by timestamp ASC
     """
 
     pipeline =  beam.Pipeline(options=pipeline_options)
