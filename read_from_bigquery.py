81922711b23b4aca598c6b838a448d0f8272d700
maxil <maxi.leitschuh@gmail.com>
2023-05-11 15:13:57 +0200

    added encode_data

--- read_from_bigquery.py
+++ read_from_bigquery.py
@@ -1,11 +1,27 @@
 import argparse
 import logging
+import json
+
+import logging
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
 def log_row(row):
-    logging.info(str(row))
+    print(row)
     return row
+
+class AddWindowInfo(beam.DoFn):
+    def process(self, x, window=beam.DoFn.WindowParam):
+        x["window_start"] = window.start.to_utc_datetime()
+        x["window_end"] = window.end.to_utc_datetime()
+        yield x
+
+
+def encode_data(data):
+    key, value = data
+    data_dict = {'pddistrict': key, 'count': value}
+    return data_dict
+
 
 def main(argv=None):
 
@@ -25,8 +41,15 @@ def main(argv=None):
     read_data = (
                 pipeline
                 | 'ReadFromBigQuery' >> beam.io.ReadFromBigQuery(query=input_query,use_standard_sql=True)
-                | "logging ginfo" >> beam.Map(log_row)
     )
+
+    incidents = (
+        read_data
+        | "Key Value Pairs" >> beam.Map(lambda x: (x["pddistrict"],1))
+        | "Sum" >> beam.CombinePerKey(sum)
+        | "logging ginfo" >> beam.Map(log_row)
+    )
+    
 
 
     result = pipeline.run()
